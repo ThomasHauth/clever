65c78c158a98a5f5bc3449608ee224c1024d2392
Daniel Funke <dfunke@funky-linux.(none)>
2012-12-04 14:13:10 +0100

    added more OpenCL functions

--- src/clever/builtin.hpp
+++ src/clever/builtin.hpp
@@ -13,6 +13,43 @@
 namespace clever
 {
 
+template <typename T>
+	struct vectorN {
+		T x, y, z, w;
+
+		vectorN(T _x = 0, T _y = 0, T _z = 0, T _w = 0) { x = _x; y = _y; z = _z; w = _w; }
+
+		vectorN operator-() const {return vectorN(-x,-y,-z,-w); }
+		vectorN operator-(vectorN p0) const {return vectorN(x - p0.x, y - p0.y, z - p0.z, w - p0.z); }
+		vectorN operator+(vectorN p0) const {return vectorN(x + p0.x, y + p0.y, z + p0.z, w + p0.z); }
+		vectorN operator*(T s) const {return vectorN(x * s, y * s, z - s, w - s); }
+		vectorN operator/(T s) const {return vectorN(x / s, y / s, z / s, w / s); }
+
+		vectorN normalize(vectorN p0) const {
+			T mag = std::sqrt(x*x + y*y + z*z + w*w);
+			return vectorN(x/mag, y/mag, z/mag, w/mag);
+		}
+
+		vectorN cross(vectorN a, vectorN b) const {
+			//only for 3 dimensions
+			return vectorN( a.y*b.z - a.z*b.y,
+							 a.z*b.x - a.x*b.z,
+							 a.x*b.y - a.y*b.x);
+		}
+	};
+
+typedef vectorN<float> floatN;
+typedef vectorN<double> doubleN;
+
+typedef floatN float2;
+typedef floatN float3;
+typedef floatN float4;
+
+typedef doubleN double2;
+typedef doubleN double3;
+typedef doubleN double4;
+
+
 class builtin : private boost::noncopyable
 {
 public:
@@ -30,6 +67,9 @@ protected:
 
     double native_sqrt( double d ){ return 0.0; }
     double native_log( double d ){ return 0.0; }
+
+    floatN cross(floatN a, floatN b) { return a.cross(a,b); }
+    floatN normalize(floatN a) { return a.normalize(a); }
 };
 
 }
