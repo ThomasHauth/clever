506bf71462e8165b910eeea3a53fd327f201f580
Daniel Funke <daniel.funke@cern.ch>
2013-01-07 12:00:01 +0100

    modified OpenCL Vector imitation

--- src/clever/builtin.hpp
+++ src/clever/builtin.hpp
@@ -13,41 +13,65 @@
 namespace clever
 {
 
+class vectorN {
+
+};
+
 template <typename T>
-	struct vectorN {
-		T x, y, z, w;
+class vector2 : public vectorN{
+public:
+	T x;
+	T y;
 
-		vectorN(T _x = 0, T _y = 0, T _z = 0, T _w = 0) { x = _x; y = _y; z = _z; w = _w; }
+	vector2(T _x, T _y) {
+		x = _x;
+		y = _y;
+	}
+};
 
-		vectorN operator-() const {return vectorN(-x,-y,-z,-w); }
-		vectorN operator-(vectorN p0) const {return vectorN(x - p0.x, y - p0.y, z - p0.z, w - p0.z); }
-		vectorN operator+(vectorN p0) const {return vectorN(x + p0.x, y + p0.y, z + p0.z, w + p0.z); }
-		vectorN operator*(T s) const {return vectorN(x * s, y * s, z - s, w - s); }
-		vectorN operator/(T s) const {return vectorN(x / s, y / s, z / s, w / s); }
+template <typename T>
+class vector3 : public vectorN{
+	T x;
+	T y;
+	T z;
 
-		vectorN normalize(vectorN p0) const {
-			T mag = std::sqrt(x*x + y*y + z*z + w*w);
-			return vectorN(x/mag, y/mag, z/mag, w/mag);
+	vector3(T _x, T _y, T _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+};
+
+template <typename T>
+class vector4 : public vectorN{
+	T x;
+	T y;
+	T z;
+	T w;
+
+	vector4(T _x, T _y, T _z, T _w) {
+			x = _x;
+			y = _y;
+			z = _z;
+			w = _w;
 		}
+};
 
-		vectorN cross(vectorN a, vectorN b) const {
-			//only for 3 dimensions
-			return vectorN( a.y*b.z - a.z*b.y,
-							 a.z*b.x - a.x*b.z,
-							 a.x*b.y - a.y*b.x);
-		}
-	};
+typedef vector2<float> float2;
+typedef vector3<float> float3;
+typedef vector4<float> float4;
 
-typedef vectorN<float> floatN;
-typedef vectorN<double> doubleN;
+typedef vector2<double> double2;
+typedef vector3<double> double3;
+typedef vector4<double> double4;
 
-typedef floatN float2;
-typedef floatN float3;
-typedef floatN float4;
+typedef vector2<int> int2;
+typedef vector3<int> int3;
+typedef vector4<int> int4;
 
-typedef doubleN double2;
-typedef doubleN double3;
-typedef doubleN double4;
+typedef vector2<uint> uint2;
+typedef vector3<uint> uint3;
+typedef vector4<uint> uint4;
 
 
 class builtin : private boost::noncopyable
@@ -62,14 +86,16 @@ protected:
     unsigned int get_local_id( unsigned int ){ return 0; }
     unsigned int get_local_size( unsigned int ){ return 0; }
 
+    unsigned int atomic_or(unsigned int * x, unsigned int a) { return (*x) ^ a; }
+
     template <class TType >
     TType atomic_inc( TType * p ) { return TType(); }
 
     double native_sqrt( double d ){ return 0.0; }
     double native_log( double d ){ return 0.0; }
 
-    floatN cross(floatN a, floatN b) { return a.cross(a,b); }
-    floatN normalize(floatN a) { return a.normalize(a); }
+    //vectorN cross(vectorN a, vectorN b) { return a.cross(b); }
+    //floatN normalize(floatN a) { return a.normalize(); }
 };
 
 }
